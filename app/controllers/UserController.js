c71eaeccb0c65d6e38852e6bcee1049b2b45eab3
LelouchLamperougee <pavel.bv2003@gmail.com>
2023-03-02 16:01:39 +0300

    Read

--- app/controllers/UserController.js
+++ app/controllers/UserController.js
@@ -4,11 +4,12 @@ class UserController{
     async createUser(req, res){
         const {nickname, pass} = req.body
         const newPerson = await db.query(`INSERT INTO users (nickname, pass) values ($1, $2) RETURNING *`, [nickname,pass])
-        res.json(newPerson)
+        res.json(newPerson.rows[0])
     }
 
     async getUsers(req, res){
-
+        const users = await db.query('SELECT * FROM users')
+        res.json(users.rows)
     }
 
     async getUser(req, res){
